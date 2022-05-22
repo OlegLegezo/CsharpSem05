409429a1f09c103c8a9bdb290ac8365ddfd695f9
OlegLegezo <oleglegezo@yandex.ru>
2022-05-22 11:08:16 +0300

    полиндром с цифрой решение 3

---
+++ ex19_v2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите 5-ти значное число");
+var number = Console.ReadLine();//var - сам определяет тип
+
+bool isPolyndrom = true;
+for (int i = 0; i < number.Lenght; i++) ;
+{
+    if (number[i] != number[number.Length - 1 - i]) ;
+    isPolyndrom = false;
+}
+Console.WriteLine(isPolyndrom);
