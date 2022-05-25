31dcc90d96f4c23403254d21396d86cccda86ce1
OlegLegezo <oleglegezo@yandex.ru>
2022-05-25 16:52:04 +0300

    палиндром числа, сравнение в стринге

--- ex19_v2/Program.cs
+++ ex19_v2/Program.cs
@@ -1,10 +1,11 @@
-﻿Console.WriteLine("введите 5-ти значное число");
+﻿Console.WriteLine("введите число");
 var number = Console.ReadLine();//var - сам определяет тип
 
 bool isPolyndrom = true;
-for (int i = 0; i < number.Lenght; i++) ;
+for (int i = 0; i < number.Length; i++)//number.Lenght - длина строки
 {
-    if (number[i] != number[number.Length - 1 - i]) ;
+    if (number[i] != number[number.Length - 1 - i]) // "!=" - не равно
     isPolyndrom = false;
 }
+
 Console.WriteLine(isPolyndrom);
